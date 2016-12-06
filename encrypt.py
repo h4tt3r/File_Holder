ece05c0a5f1a465a36e6085b143d098aee6ceb4f
h4tt3r <theblackfox5066@gmail.com>
2016-12-06 01:22:06 +0000

    Add files via upload

---
+++ encrypt.py
@@ -0,0 +1,73 @@
+clearMes = raw_input("Enter a message to be encrypted:\n> ") 
+
+
+alphabet = [' ' ,'a', 'b', 'c', 
+			'd', 'e', 'f', 
+			'g', 'h', 'i', 
+			'j', 'k', 'l', 
+			'm','n', 'o',
+			'p', 'q', 'r', 
+			's', 't', 'u',
+			'v', 'w', 'x', 
+			'y', 'z', 'A', 
+			'B', 'C', 'D', 
+			'E', 'F', 'G', 
+			'H', 'I', 'J', 
+			'K', 'L', 'M', 
+			'N', 'O', 'P', 
+			'Q', 'R', 'S', 
+			'T', 'U', 'V', 
+			'W', 'X', 'Y', 
+			'Z']
+			
+clearMesList = []
+numList = []
+
+#Puts all characters in the clear message into a list 
+for x in clearMes:
+	clearMesList.append(x)
+
+#Compares clear list to alphabet
+for x in clearMesList:
+	y = 1
+	
+	#Checks to see if message character is equal to alphabet letter 
+	while x != alphabet[y]:
+		y = y + 1
+		#If the character is not a letter, then it is represented by a 0
+		if y > 46:
+			y = 0
+			break
+	#Adds now converted character to numList 
+	numList.append(y)
+
+
+numList2 = [] 
+
+#Does math to all the coverted characters
+for x in numList:
+	x = x * 9
+	x = x + 504
+	x = x - 62
+	x = x + x*34+21
+	x = x / 12 
+	numList2.append(x)
+	
+
+y = 0 
+#Converts all ints in numList2 into strings
+for x in numList2:
+	numList2[y] = str(numList2[y])
+	y = y+1
+
+
+#Joins all numbers in a list into a string 
+encryptedstring = "".join(numList2)
+
+print "\nEncrypted message:\n" + encryptedstring
+
+
+
+
+
+
